a4ea91037dd438e76b808ef83fabb808805bafad
DESKTOP-6D1INUT\Николай <domnik@mail.ru>
2024-09-26 13:40:20 +0300

    RabbitMQ Receiver

---
+++ src/main/java/com/ngprofi/rabbitmq_reciever/RabbitmqRecieverApplication.java
@@ -0,0 +1,13 @@
+package com.ngprofi.rabbitmq_reciever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqRecieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqRecieverApplication.class, args);
+	}
+
+}
